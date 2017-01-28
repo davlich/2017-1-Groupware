a8e57b13bd914f7305f051d8cd0113bb6f8e2826
laboratorio <laboratorio@LAB09-21>
2017-01-28 16:35:42 -0600

    se agrega proyecto de logica de negocio para la calculadora

---
+++ Ulatina.Topicos.Calentamiento/Ulatina.Topicos.Calculadora/Domain/Actions/Multiplicar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ulatina.Topicos.Calculadora.Domain.Actions
+{
+    public class Multiplicar
+    {
+    }
+}
---
+++ Ulatina.Topicos.Calentamiento/Ulatina.Topicos.Calculadora/Domain/Actions/Sumar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ulatina.Topicos.Calculadora.Domain.Actions
+{
+    public class Sumar
+    {
+
+    }
+}
